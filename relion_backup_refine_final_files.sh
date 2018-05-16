fafcd867e39fdedf443409b5b241eb0ddcca82f7
kylelmorris <kylelmorris@gmail.com>
2018-05-16 11:12:36 -0700

    18May16

--- relion_backup_refine_final_files.sh
+++ relion_backup_refine_final_files.sh
@@ -11,7 +11,7 @@ if [[ -z $1 ]] || [[ -z $2 ]] ; then
   echo "Variables empty, usage is ${0} (1) (2)"
   echo ""
   echo "(1) = Refine3D directory in"
-  echo "(2) = rsync backup directory out"
+  echo "(2) = Backup directory out"
   echo "(3) = port number for scp (optional)"
   exit
 
@@ -22,14 +22,14 @@ if [[ -z $3 ]] ; then
 fi
 
 #Write a README to the backup location with the original location of the files
-echo "host information:" > $dirout/README
-hostname -s >> $dirout/README
-hostname -I >> $dirout/README
-echo "" >> $dirout/README
-echo "Refine3D location" >> $dirout/README
-echo $(ls -d -1 $PWD/$dirin) >> $dirout/README
-echo "" >> $dirout/README
-cat $dirin/note.txt >> $dirout/README
+echo "host information:" > $dirin/README
+hostname -s >> $dirin/README
+hostname -I >> $dirin/README
+echo "" >> $dirin/README
+echo "Refine3D location" >> $dirin/README
+echo $(ls -d -1 $PWD/$dirin) >> $dirin/README
+echo "" >> $dirin/README
+cat $dirin/note.txt >> $dirin/README
 
 #Copy files from specified Refine3D directory to backup location
 scp -P $port -r $dirin/run.job $dirout
@@ -44,3 +44,8 @@ scp -P $port -r $dirin/run_half1_class001_unfil.mrc $dirout
 scp -P $port -r $dirin/run_half2_class001_unfil.mrc $dirout
 scp -P $port -r $dirin/*pipeline* $dirout
 scp -P $port -r $dirin/note.txt $dirout
+scp -P $port -r $dirin/README $dirout
+
+rm -rf $dirin/README
+
+echo "Done!"
