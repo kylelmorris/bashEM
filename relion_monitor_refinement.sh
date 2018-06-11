8bc36e1e522dc07560271508c73ffad7ec7f1efb
kylelmorris <kylelmorris@gmail.com>
2018-06-10 17:31:22 -0700

    18Jun10

---
+++ relion_monitor_refinement.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+
+jobin=$1
+lines=$2
+
+if [[ -z $1 ]] ; then
+
+  echo ""
+  echo "Variables empty, usage is ${0} (1) (2)"
+  echo ""
+  echo "(1) = job directory name"
+  echo "(2) = Number of output lines (optional)"
+  exit
+
+fi
+
+if [[ -z $lines ]] ; then
+  lines=10
+fi
+
+
+tail -f -n ${lines} ./Refine3D/${jobin}/run.out
