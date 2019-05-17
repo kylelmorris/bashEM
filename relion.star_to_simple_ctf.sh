ce8e511fcce25d303854c8c773b47aa9c93b4043
Kyle Morris <kylelmorris@gmail.com>
2019-05-17 09:29:17 +0100

    19May17

--- relion.star_to_simple_ctf.sh
+++ relion.star_to_simple_ctf.sh
@@ -25,11 +25,12 @@
 # Assumes you have a single stack and star file from relion ready for phase flipping in Simple
 
 
-if [[ -z $1 ]] ; then
+if [[ -z $1 ]] | [[ -z $2 ]] ; then
   echo ""
   echo "Variables empty, usage is relion_star_to_simple_ctf (1) (2)"
   echo ""
   echo "(1) = star file"
+  echo "(2) = output dirctory name"
   echo ""
   exit
 fi
@@ -38,6 +39,8 @@ fi
 starin=$1
 name=$(basename $starin .star)
 out=$name
+outdir=$2
+simpledir="./Simple/Extract"
 
 ###############################################################################
 # Are dependancies present?
@@ -95,9 +98,9 @@ echo "rlnAmplitudeContrast:    ${ampcol1}"
 echo ""
 
 ## Create new directory for creating simple file in
-mkdir simple
+mkdir -p ${simpledir}/${outdir}
 echo "Created directory for simple file output..."
-echo ""
+echo "${simpledir}/${outdir}"
 
 ## Get defocusU/V and astig columns without the header of the star file and place in simple formatted ctf params file
 # Note that as of Relion-3.0 version header needs removing
@@ -107,8 +110,8 @@ cat $starin | grep -v "# RELION" | awk \
 'BEGIN {i=1} {if (NF<3) {i++} else {print "kv="$c1,"cs="$c2,"fraca="$c3,"dfx="$c4/10000,"dfy="$c5/10000,"angast="$11}}' > deftab.txt
 ptclno=$(wc -l deftab.txt | awk '{print $1}')
 
-mv deftab.txt simple
-echo "Created Simple formatted deftab.txt"
+mv deftab.txt ${simpledir}/${outdir}
+echo "Created Simple formatted deftab.txt in output directory"
 echo ""
 
 ## Get microscope and data set parameters
@@ -117,7 +120,7 @@ cs1=$(cat $starin | grep -v "# RELION" | awk -v c1="$cscol1" 'BEGIN {i=1} {if (N
 amp1=$(cat $starin | grep -v "# RELION" | awk -v c1="$ampcol1" 'BEGIN {i=1} {if (NF<3) {i++} else {print $c1}}' | head -n 1)
 dstep=$(cat $starin | grep -v "# RELION" | awk -v c1="$dstepcol1" 'BEGIN {i=1} {if (NF<3) {i++} else {print $c1}}' | head -n 1)
 mag=$(cat $starin | grep -v "# RELION" | awk -v c1="$magcol1" 'BEGIN {i=1} {if (NF<3) {i++} else {print $c1}}' | head -n 1)
-ptclno=$(wc -l simple/deftab.txt | awk {'print $1'})
+ptclno=$(wc -l ${simpledir}/${outdir}/deftab.txt | awk {'print $1'})
 
 # Calculate pixel size
 apix=$(bc <<< "scale=3; ${dstep}*10000/${mag}")
@@ -131,55 +134,23 @@ echo "Ptcl no: ${ptclno}"
 echo ""
 
 ## Save a useful file to pull info from
-echo "Simple parameters" > simple/.params.dat
-echo "Voltage: ${kv1}" >> simple/.params.dat
-echo "Cs (mm): ${cs1}" >> simple/.params.dat
-echo "AmpC:    ${amp1}" >> simple/.params.dat
-echo "Apix:    ${apix}" >> simple/.params.dat
-echo "Ptcl no: ${ptclno}" >> simple/.params.dat
+echo "Simple parameters" > ${simpledir}/${outdir}/.params.dat
+echo "Voltage: ${kv1}" >> ${simpledir}/${outdir}/.params.dat
+echo "Cs (mm): ${cs1}" >> ${simpledir}/${outdir}/.params.dat
+echo "AmpC:    ${amp1}" >> ${simpledir}/${outdir}/.params.dat
+echo "Apix:    ${apix}" >> ${simpledir}/${outdir}/.params.dat
+echo "Ptcl no: ${ptclno}" >> ${simpledir}/${outdir}/.params.dat
 
 ## Use relion to create a single stack of the particles
 echo "Using Relion to create a single stack with and without phase flipping..."
 echo ""
-relion_preprocess --operate_on ${starin} --reextract_data_star --operate_out simple/particles_pflip --premultiply_ctf --phase_flip
+relion_preprocess --operate_on ${starin} --reextract_data_star --operate_out ${simpledir}/${outdir}/particles_pflip --premultiply_ctf --phase_flip
 echo "Created phase flipped particles..."
 echo ""
-relion_preprocess --operate_on ${starin} --reextract_data_star --operate_out simple/particles
+relion_preprocess --operate_on ${starin} --reextract_data_star --operate_out ${simpledir}/${outdir}/particles
 echo "Created normal particles..."
 echo ""
 #relion_stack_create --i ${starin} --o simple/${name}
-
-echo ""
-echo "Done!"
-echo ""
-
-
-exit
-
-
-###### Depreciated code
-
-
-## Create a new star file which has only the defocus, astig and pixel size params in reference to ctf corrected stack
-### Create data columns for single stack of particles with same name as input star file
-relion_star_datablock_stack $ptclno $out"_phflip.mrcs" > simple/datablock_ImageName
-### Get data columns from star file
-relion_star_printtable $starin data rlnDefocusU rlnDefocusV rlnDefocusAngle rlnDetectorPixelSize rlnMagnification rlnAmplitudeContrast rlnVoltage rlnSphericalAberration > simple/datablock_data
-### Create new header for data columns
-relion_star_loopheader rlnImageName rlnDefocusU rlnDefocusV rlnDefocusAngle rlnDetectorPixelSize rlnMagnification rlnAmplitudeContrast rlnVoltage rlnSphericalAberration > simple/header
-### Combine data together into new star file
-paste datablock_ImageName simple/datablock_data | awk {'print $1,$2,$3,$4,$5,$6,$7,$8,$9'} > simple/datablock
-cat simple/header simple/datablock > simple/$out"_phflip.star"
-echo ""
-echo "Created Relion formatted ctfparams.star referring to input stack:" $name.mrcs
-
-exit
-
-# Tidy up
-rm -rf datablock
-rm -rf datablock_ImageName
-rm -rf datablock_data
-rm -rf header
 
 # Finish
 echo ""
