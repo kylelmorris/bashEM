eececfb6ec8dd89c46ccc42a01f9f2ae50d48d8a
Kyle Morris <kylelmorris@gmail.com>
2019-05-22 14:08:03 +0100

    19May22

--- relion.select_classes.sh
+++ relion.select_classes.sh
@@ -3,6 +3,7 @@
 
 # Variables
 stackin=$1
+output=stack_selection
 
 # Test if Relion is sourced and available
 command -v relion >/dev/null 2>&1 || { echo >&2 "Relion does not appear to be installed or loaded..."; exit 1; }
@@ -26,28 +27,28 @@ name=$(basename $stackin .${ext})
 dir=$(dirname $stackin)
 
 # Make directory for class occupancy data and plots
-mkdir -p ${dir}/stack_selection
+mkdir -p ${dir}/${output}
 
 # Copy stack into selection directory
-scp -r ${stackin} ${dir}/stack_selection/${name}.mrcs
+scp -r ${stackin} ${dir}/${output}/${name}.mrcs
 
 ## Make model star file
 # Make model header
-relion_star_loopheader _rlnReferenceImage _rlnClassPriorOffsetX _rlnClassPriorOffsetY > ${dir}/${name}_model_header.star
-sed -i 's/data_/data_model_classes/g' ${dir}/${name}_model_header.star
+relion_star_loopheader rlnReferenceImage rlnClassPriorOffsetX rlnClassPriorOffsetY > ${dir}/${output}/${name}_model_header.star
+sed -i 's/data_/data_model_classes/g' ${dir}/${output}/${name}_model_header.star
 # Make model datalines
-imgno=$(relion_image_handler --stats --i ${dir}/stack_selection/${name}.mrcs | wc -l)
-relion_star_datablock_stack ${imgno} ${name}.mrcs 0.000000 0.000000 > ${dir}/${name}_model_datalines.star
+imgno=$(relion_image_handler --stats --i ${dir}/${output}/${name}.mrcs | wc -l)
+relion_star_datablock_stack ${imgno} ${dir}/${output}/${name}.mrcs 0.000000 0.000000 > ${dir}/${output}/${name}_model_datalines.star
 # Combine header and datalines
-cat ${dir}/${name}_model_header.star ${dir}/${name}_model_datalines.star > ${dir}/${name}_model.star
-rm -rf ${dir}/${name}_model_header.star
-rm -rf ${dir}/${name}_model_datalines.star
+cat ${dir}/${output}/${name}_model_header.star ${dir}/${output}/${name}_model_datalines.star > ${dir}/${output}/${name}_model.star
+rm -rf ${dir}/${output}/${name}_model_header.star
+rm -rf ${dir}/${output}/${name}_model_datalines.star
 
 # Make dummy particle data.star file
-echo data_ > ${dir}/${name}_data.star
+echo data_ > ${dir}/${output}/${name}_data.star
 
 # Allow user to select images from stack using relion_display
-relion_display --gui --i ${dir}/${name}_model.star --allow_save --fn_imgs ${dir}/stack_selection/stack_selection.star --recenter
+relion_display --gui --i ${dir}/${output}/${name}_model.star --allow_save --fn_imgs ${dir}/${output}/stack_selection.star --recenter
 
 # Finish
 echo ""
