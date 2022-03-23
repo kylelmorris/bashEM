d7601bf954613ec3aa1a0c41709d3939f9170fc7
Kyle Morris <kylelmorris@gmail.com>
2022-03-23 10:03:25 +0000

    22Mar23

--- relion.star_info.sh
+++ relion.star_info.sh
@@ -70,28 +70,28 @@ versionSearch=$(cat ${dirout}/.version.dat)
 
 #Calculate number of particles by data lines minus header
 totallines=$(wc -l $starin | awk {'print $1'})
-#mainHeaderLines=$(wc -l .mainDataHeader.dat | awk {'print $1'})
-mainDataLines=$(wc -l ${dirout}/.mainDataLines.dat | awk {'print $1'})
-#opticsHeaderLines=$(wc -l .opticsDataHeader.dat | awk {'print $1'})
-opticsLines=$(wc -l ${dirout}/.opticsDataLines.dat | awk {'print $1'})
+#mainHeaderLines=$(wc -l mainDataHeader.dat | awk {'print $1'})
+mainDataLines=$(wc -l ${dirout}/mainDataLines.dat | awk {'print $1'})
+#opticsHeaderLines=$(wc -l opticsDataHeader.dat | awk {'print $1'})
+opticsLines=$(wc -l ${dirout}/opticsDataLines.dat | awk {'print $1'})
 
 #Calculate number of micrographs
 columnname=rlnMicrographName
-column=$(grep ${columnname} ${dirout}/.mainDataHeader.dat | awk '{print $2}' | sed 's/#//g')
+column=$(grep ${columnname} ${dirout}/mainDataHeader.dat | awk '{print $2}' | sed 's/#//g')
 #echo $columnname 'is column number:' $column
-miclines=$(awk -v column=$column '{print $column}' ${dirout}/.mainDataLines.dat | sort -u | wc -l | awk {'print $1'})
+miclines=$(awk -v column=$column '{print $column}' ${dirout}/mainDataLines.dat | sort -u | wc -l | awk {'print $1'})
 
 #Calculate number of image groups
 columnname=rlnGroupNumber
-column=$(grep ${columnname} ${dirout}/.mainDataHeader.dat | awk '{print $2}' | sed 's/#//g')
+column=$(grep ${columnname} ${dirout}/mainDataHeader.dat | awk '{print $2}' | sed 's/#//g')
 #echo $columnname 'is column number:' $column
-grouplines=$(awk -v column=$column '{print $column}' ${dirout}/.mainDataLines.dat | sort -u | wc -l | awk {'print $1'})
+grouplines=$(awk -v column=$column '{print $column}' ${dirout}/mainDataLines.dat | sort -u | wc -l | awk {'print $1'})
 
 #Calculate number of classes
 columnname=rlnClassNumber
-column=$(grep ${columnname} ${dirout}/.mainDataHeader.dat | awk '{print $2}' | sed 's/#//g')
+column=$(grep ${columnname} ${dirout}/mainDataHeader.dat | awk '{print $2}' | sed 's/#//g')
 #echo $columnname 'is column number:' $column
-classlines=$(awk -v column=$column '{print $column}' ${dirout}/.mainDataLines.dat | sort -u | wc -l | awk {'print $1'})
+classlines=$(awk -v column=$column '{print $column}' ${dirout}/mainDataLines.dat | sort -u | wc -l | awk {'print $1'})
 
 #Calculate ptcls per micrograph in star file
 ptclpermic=$(bc <<< "scale=0; ${mainDataLines}/${miclines}")
@@ -101,26 +101,26 @@ ptclperclass=$(bc <<< "scale=0; ${mainDataLines}/${classlines}")
 
 #Find cs (mm)
 columnname=rlnSphericalAberration
-column=$(grep ${columnname} ${dirout}/.opticsDataHeader.dat | awk '{print $2}' | sed 's/#//g')
+column=$(grep ${columnname} ${dirout}/opticsDataHeader.dat | awk '{print $2}' | sed 's/#//g')
 #echo $columnname 'is column number:' $column
-cs=$(awk -v column=$column '{print $column}' ${dirout}/.opticsDataLines.dat | head -n 1)
+cs=$(awk -v column=$column '{print $column}' ${dirout}/opticsDataLines.dat | head -n 1)
 
 #Calculate pixel size, note micrograph and particle files use rlnMicrographPixelSize and rlnImagePixelSize respectively
 columnname=ImagePixelSize
 #Assess whether this is a micrograph or particle star file
-columnEntry=$(grep ${columnname} ${dirout}/.opticsDataHeader.dat |  awk '{print $1}')
+columnEntry=$(grep ${columnname} ${dirout}/opticsDataHeader.dat |  awk '{print $1}')
 
-column=$(grep ${columnname} ${dirout}/.opticsDataHeader.dat | awk '{print $2}' | sed 's/#//g')
+column=$(grep ${columnname} ${dirout}/opticsDataHeader.dat | awk '{print $2}' | sed 's/#//g')
 #echo $columnname 'is column number:' $column
-apix=$(awk -v column=$column '{print $column}' ${dirout}/.opticsDataLines.dat | head -n 1)
+apix=$(awk -v column=$column '{print $column}' ${dirout}/opticsDataLines.dat | head -n 1)
 
 #Get defocus column and calculate minimum and maximum defocus
 columnname=rlnDefocusU
-column=$(grep ${columnname} ${dirout}/.mainDataHeader.dat | awk '{print $2}' | sed 's/#//g')
+column=$(grep ${columnname} ${dirout}/mainDataHeader.dat | awk '{print $2}' | sed 's/#//g')
 #echo $columnname 'is column number:' $column
-tmp=$(awk -v column=$column '{print $column}' ${dirout}/.mainDataLines.dat | awk NF | sort -n | head -n 1)
+tmp=$(awk -v column=$column '{print $column}' ${dirout}/mainDataLines.dat | awk NF | sort -n | head -n 1)
 mindf=$(bc <<< "scale=0; ${tmp}/10")
-tmp=$(awk -v column=$column '{print $column}' ${dirout}/.mainDataLines.dat | awk NF | sort -n | tail -n 1)
+tmp=$(awk -v column=$column '{print $column}' ${dirout}/mainDataLines.dat | awk NF | sort -n | tail -n 1)
 maxdf=$(bc <<< "scale=0; ${tmp}/10")
 
 echo ''
